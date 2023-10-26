738ad561cfa882c2c03994249b925739f2fb337c
piotek8 <piotr.bator.contact@gmail.com>
2023-10-26 19:16:20 +0200

    feat/base-program: corrected constants

--- app/constants.py
+++ app/constants.py
@@ -1,10 +1,2 @@
-#INPUT_PATH = "sources/base"
-#OUTPUT_PATH = "sources/translated"
-#
-#
-#INPUT_PATH = r"C:\Users\Gaming\Desktop\Projects\AI\script\sources\base"
-#OUTPUT_PATH = r"C:\Users\Gaming\Desktop\Projects\AI\script\sources\translated"
-
-#INPUT_PATH = "C:\\Users\\Gaming\\Desktop\\Projects\\AI\\script\\sources\\base\\translated_chapter-01-wybor-jezyka.md"
-INPUT_PATH = "C:\\Users\\Gaming\\Desktop\\Projects\\AI\\script\\sources\\base"
-OUTPUT_PATH = "C:\\Users\\Gaming\\Desktop\\Projects\\AI\\script\\sources\\translated"
+INPUT_PATH = "sources/base"
+OUTPUT_PATH = "sources/translated"
